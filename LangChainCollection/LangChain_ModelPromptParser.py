12bc5ef1bde8186b9ebaf6c497566f7cb0afb287
Mohsen Nayebi <122830808+mohsen-nyb@users.noreply.github.com>
2025-02-18 17:29:34 -0600

    Create LangChain_ModelPromptParser.py

---
+++ LangChainCollection/LangChain_ModelPromptParser.py
@@ -0,0 +1,145 @@
+#adapted from "LangChain for LLM Application Development" from "DeepLearning.AI".
+
+
+#!pip install --upgrade langchain
+#!pip install -U langchain-community
+
+
+
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+
+chat = ChatOpenAI(temperature=0, model=llm_model) # temperature controls the randomness of the model’s responses.
+
+template_string = """Translate the text \
+that is delimited by triple backticks \
+into a style that is {style}. \
+text: ```{text}```
+"""
+
+prompt_template = ChatPromptTemplate.from_template(template_string)
+print(prompt_template.messages[0].prompt.input_variables) # ["style", "text"]
+
+customer_style = """American English \
+in a calm and respectful tone
+"""
+
+customer_email = """
+Arrr, I be fuming that me blender lid \
+flew off and splattered me kitchen walls \
+with smoothie! And to make matters worse, \
+the warranty don't cover the cost of \
+cleaning up me kitchen. I need yer help \
+right now, matey!
+"""
+
+customer_messages = prompt_template.format_messages(
+    style=customer_style,
+    text=customer_email
+)
+
+# Call the LLM to translate to the style of the customer message
+customer_response = chat(customer_messages)
+
+print(customer_response.content)
+
+
+##########################################################################################
+
+customer_review = """\
+This leaf blower is pretty amazing.  It has four settings:\
+candle blower, gentle breeze, windy city, and tornado. \
+It arrived in two days, just in time for my wife's \
+anniversary present. \
+I think my wife liked it so much she was speechless. \
+So far I've been the only one using it, and I've been \
+using it every other morning to clear the leaves on our lawn. \
+It's slightly more expensive than the other leaf blowers \
+out there, but I think it's worth it for the extra features.
+"""
+
+review_template = """\
+For the following text, extract the following information:
+
+gift: Was the item purchased as a gift for someone else? \
+Answer True if yes, False if not or unknown.
+
+delivery_days: How many days did it take for the product \
+to arrive? If this information is not found, output -1.
+
+price_value: Extract any sentences about the value or price,\
+and output them as a comma separated Python list.
+
+Format the output as JSON with the following keys:
+gift
+delivery_days
+price_value
+
+text: {text}
+"""
+
+prompt_template = ChatPromptTemplate.from_template(review_template)
+messages = prompt_template.format_messages(text=customer_review)
+
+response = chat(messages)
+print(response.content)
+
+
+# Parse the LLM output string into a Python dictionary
+
+from langchain.output_parsers import ResponseSchema, StructuredOutputParser
+
+gift_schema = ResponseSchema(name="gift",
+                             description="Was the item purchased\
+                             as a gift for someone else? \
+                             Answer True if yes,\
+                             False if not or unknown.")
+delivery_days_schema = ResponseSchema(name="delivery_days",
+                                      description="How many days\
+                                      did it take for the product\
+                                      to arrive? If this \
+                                      information is not found,\
+                                      output -1.")
+price_value_schema = ResponseSchema(name="price_value",
+                                    description="Extract any\
+                                    sentences about the value or \
+                                    price, and output them as a \
+                                    comma separated Python list.")
+
+response_schemas = [gift_schema, 
+                    delivery_days_schema,
+                    price_value_schema]
+
+output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+format_instructions = output_parser.get_format_instructions()
+
+
+review_template_2 = """\
+For the following text, extract the following information:
+
+gift: Was the item purchased as a gift for someone else? \
+Answer True if yes, False if not or unknown.
+
+delivery_days: How many days did it take for the product\
+to arrive? If this information is not found, output -1.
+
+price_value: Extract any sentences about the value or price,\
+and output them as a comma separated Python list.
+
+text: {text}
+
+{format_instructions}
+"""
+
+prompt = ChatPromptTemplate.from_template(template=review_template_2)
+
+messages = prompt.format_messages(text=customer_review, 
+                                format_instructions=format_instructions)
+
+print(messages[0].content)
+
+
+response = chat(messages)
+output_dict = output_parser.parse(response.content)
+print(output_dict)
+print(output_dict.get('delivery_days'))
